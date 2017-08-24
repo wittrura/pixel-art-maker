6424a1a053ad7a2fd8f64d217739314669c72ff7
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-08-24 13:15:59 -0500

    Refactors HTML to build in javasript, adds palette colors

--- app.js
+++ app.js
@@ -2,7 +2,14 @@
 
   // config
   let config = {
-    countPixels: 100
+    countPixels: 864,
+    paletteColors: ["limegreen", "palegreen", "forestgreen","darkgreen", "yellow",
+                    "khaki","coral","orange","orangered","firebrick","lightsalmon","lightpink",
+                    "palevioletred","crimson","red","darkred","darkmagenta","purple","indigo",
+                    "lavender","slateblue","darkblue","mediumblue","dodgerblue","powderblue",
+                    "lightskyblue","cadetblue","lightcyan","white","grey","black","bisque",
+                    "burlywood","tan","saddlebrown"
+                  ]
   };
 
 
@@ -21,7 +28,7 @@
     updateActiveColor: function(color) {
       if (color) {
         model.brushColor = color;
-        document.getElementById('pallete-current-color').style.backgroundColor = color;
+        document.getElementById('palette-current-color').style.backgroundColor = color;
       }
     },
 
@@ -43,7 +50,7 @@
     },
 
     buildPixelGrid: function() {
-      let canvas = this.getCanvas()
+      let canvas = this.getCanvas();
 
       for (var i = 0; i < config.countPixels; i++) {
         let pixelDiv = document.createElement('div');
@@ -52,18 +59,37 @@
       }
     },
 
+    buildPalette: function() {
+      let palette = document.getElementById('palette');
+      let colors = config.paletteColors;
+      colors.forEach(function(color) {
+        let colorSwatch = document.createElement('div');
+        colorSwatch.className = 'palette-color';
+        colorSwatch.style.backgroundColor = color;
+        palette.appendChild(colorSwatch);
+      });
+
+      let span = document.createElement('span');
+      span.innerText = 'CURRENT COLOR ->';
+      palette.appendChild(span);
+
+      let currentColor = document.createElement('div');
+      currentColor.id = 'palette-current-color';
+      palette.appendChild(currentColor);
+    },
 
     init: function() {
       this.buildPixelGrid();
+      this.buildPalette();
 
       let canvas = this.getCanvas();
       canvas.addEventListener('click', function(e) {
         controller.paintPixel(e.target);
       });
 
-      let pallete = document.getElementById('pallete');
-      pallete.addEventListener('click', function(e) {
-        controller.updateActiveColor(e.target.dataset.color);
+      let palette = document.getElementById('palette');
+      palette.addEventListener('click', function(e) {
+        controller.updateActiveColor(e.target.style.backgroundColor);
       });
 
     }
